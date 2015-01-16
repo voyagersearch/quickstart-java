3bde46fda4909cee1b8355f51065fb803d30580d
ryan <ryantxu@gmail.com>
2015-01-15 22:43:42 -0800

    register an extractor with a format explicitly

--- src/main/java/voyager/quickstart/extractor/simple/SimpleTextExtractorFactory.java
+++ src/main/java/voyager/quickstart/extractor/simple/SimpleTextExtractorFactory.java
@@ -3,6 +3,8 @@ package voyager.quickstart.extractor.simple;
 import java.util.Locale;
 
 import voyager.api.discovery.extractor.Extractor;
+import voyager.api.discovery.info.ExtractorInfoBean;
+import voyager.api.discovery.info.FormatInfoBean;
 import voyager.discovery.extractor.info.AbstractJavaExtractorFactory;
 
 public class SimpleTextExtractorFactory extends AbstractJavaExtractorFactory {
@@ -15,6 +17,19 @@ public class SimpleTextExtractorFactory extends AbstractJavaExtractorFactory {
   @Override
   public String getExtractorName() {
     return "quickstart";
+  }
+
+  /**
+   * Explicitly map this Extractor to a known mime-type
+   */
+  @Override
+  public ExtractorInfoBean getInfoBean() {
+    ExtractorInfoBean info = super.getInfoBean();
+    FormatInfoBean b = new FormatInfoBean();
+    b.setMime("text/plain");
+    b.setPriority((byte)10);
+    info.add(b);
+    return info;
   }
   
   @Override
